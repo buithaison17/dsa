2f27ea86eaf9d50bab9bd18bc524e01cf5ec59d4
Son Bui <thaisonb936@gmail.com>
2025-07-08 21:02:38 +0700

    upload dsa bt10 lession13

--- session13/PTIT_CNTT2_IT201_Session13_Bai09/baitap.c
+++ session13/PTIT_CNTT2_IT201_Session13_Bai09/baitap.c
@@ -5,21 +5,21 @@
 
 int top = -1;
 
-void push(char stack[][MAX], char data[]){
+void push(char stack[][MAX], char value[]){
     if(top >= MAX - 1) return;
     top++;
-    strcpy(stack[top], data);
+    strcpy(stack[top], value);
 }
 
 char* pop(char stack[][MAX]){
-    char* data = stack[top];
+    if(top == -1) return "";
+    char* value = stack[top];
     top--;
-    return data;
+    return value;
 }
 
-void converToInfix(char stack[][MAX], char postfix[]){
+void convertToInfix(char stack[][MAX], char postfix[], char infix[]){
     int length = strlen(postfix);
-    char result[MAX];
     for(int i = 0; i < length; i++){
         if(isdigit(postfix[i])){
             char temp[2] = {postfix[i], '\0'};
@@ -28,19 +28,18 @@ void converToInfix(char stack[][MAX], char postfix[]){
         else if(postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/'){
             char* a = pop(stack);
             char* b = pop(stack);
-            sprintf(result, "(%s %c %s)", b, postfix[i], a);
-            push(stack, result);
+            sprintf(infix, "(%s %c %s)", b, postfix[i], a);
+            push(stack, infix);
         }
     }
-    printf("Infix: %s", result);
 }
 
 int main(){
-    char stack[MAX][MAX];
-    char postfix[MAX];
+    char stack[MAX][MAX], postfix[MAX], infix[MAX];
     printf("Postfix: ");
     fgets(postfix, MAX, stdin);
     postfix[strcspn(postfix, "\n")] = '\0';
-    converToInfix(stack, postfix);
+    convertToInfix(stack, postfix, infix);
+    printf("Infix: %s", infix);
     return 0;
 }
---
+++ session13/PTIT_CNTT2_IT201_Session13_Bai10/baitap.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define MAX 100
+
+int top = -1;
+
+void push(char stack[], char infix){
+    if(top >= MAX - 1) return;
+    top++;
+    stack[top] = infix;
+}
+
+char pop(char stack[]){
+    if(top == -1) return '\0';
+    char value = stack[top];
+    top--;
+    return value;
+}
+
+int compareOperator(char op){
+    if(op == '+' || op == '-') return 1;
+    if(op == '*' || op == '/') return 2;
+    return 0;
+}
+
+void convertToPostfix(char stack[], char infix[], char postfix[]){
+    int length = strlen(infix);
+    int index = 0;
+    for(int i = 0; i < length; i++){
+        char ch = infix[i];
+        if(isdigit(ch)){
+            postfix[index++] = ch;
+        }
+        else if(ch == '('){
+            push(stack, ch);
+        }
+        else if(ch == ')'){
+            while(top != -1 && stack[top] != '('){
+                postfix[index++] = pop(stack);
+            }
+            pop(stack);
+        }
+        else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+            if(top != -1 && compareOperator(stack[top]) >= compareOperator(ch)){
+                postfix[index++] = pop(stack);
+            }
+            push(stack, ch);
+        }
+    }
+    while(top != -1){
+        postfix[index++] = pop(stack);
+    }
+    postfix[index] = '\0';
+}
+
+int main(){
+    char stack[MAX], infix[MAX], postfix[MAX];
+    printf("Infix: ");
+    fgets(infix, MAX, stdin);
+    infix[strcspn(infix, "\n")] = '\0';
+    convertToPostfix(stack, infix, postfix);
+    printf("Postfix: %s", postfix);
+    return 0;
+}
